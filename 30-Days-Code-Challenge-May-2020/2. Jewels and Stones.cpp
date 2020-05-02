70233ff582e26e1a283b2921f7a5f95041308616
Abanoub-Asaad <abanoub.asaad200010@gmail.com>
2020-05-02 12:38:25 +0200

    add links to problem statement

--- 30-Days-Code-Challenge-May-2020/2. Jewels and Stones.cpp
+++ 30-Days-Code-Challenge-May-2020/2. Jewels and Stones.cpp
@@ -1,20 +1,22 @@
+//Problem link : https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3317/
+
 class Solution {
-    
+
 public:
-    
+
     int numJewelsInStones(string J, string S) {
-        
+
         set <char> set_chars_J ;
         int cnt=0 ;
-        
+
         for(int i=0; i<J.size(); i++)
             set_chars_J.insert(J[i]);
-        
+
         for(int i=0; i<S.size(); i++)
             if(set_chars_J.count(S[i]))
                 ++cnt ;
-        
+
         return cnt ;
-            
+
     }
 };
