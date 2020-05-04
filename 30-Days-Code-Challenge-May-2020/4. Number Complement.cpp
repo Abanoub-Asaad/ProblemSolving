d441981edc618cb79bea3e7dea4e807eb59c7d41
Abanoub-Asaad <abanoub.asaad200010@gmail.com>
2020-05-04 14:30:47 +0200

    Add solution to problem 4

---
+++ 30-Days-Code-Challenge-May-2020/4. Number Complement.cpp
@@ -0,0 +1,28 @@
+// Problem Link : https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3319/
+
+class Solution {
+public:
+    int findComplement(int num) {
+
+    int output=0 ;
+
+    /*
+       Complement_num_str is a string for the complement but it's the inverse of it
+       to ease the tracking process.
+    */
+    string Complement_num_str="";
+
+
+        while(num!=0){
+            if(!(num%2))
+                Complement_num_str+='1', num/=2;
+            else
+                Complement_num_str+='0', num/=2;
+        }
+
+       for(int i=0; i<Complement_num_str.size(); i++)
+            if(Complement_num_str[i]=='1') output+=pow(2,i);
+
+        return output;
+    }
+};
