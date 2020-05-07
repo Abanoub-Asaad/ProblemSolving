84882bc550d9c27075b978bd8eae3d3007f7e4a9
Abanoub-Asaad <abanoub.asaad200010@gmail.com>
2020-05-07 14:38:16 +0200

    Add solution to problem 7

---
+++ 30-Days-Code-Challenge-May-2020/7. Cousins in Binary Tree.java
@@ -0,0 +1,40 @@
+//Problem Link : https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3322/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    Map<Integer, Integer> depth;
+    Map<Integer, TreeNode> parent;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        
+        depth = new HashMap();
+        parent = new HashMap();
+        
+        dfs(root, null);
+        return (depth.get(x) == depth.get(y) && parent.get(x) != parent.get(y));
+    }
+
+    public void dfs(TreeNode node, TreeNode par) {
+        if (node != null) {
+            depth.put(node.val, par != null ? 1 + depth.get(par.val) : 0);
+            parent.put(node.val, par);
+            dfs(node.left, node);
+            dfs(node.right, node);
+        }
+    }
+}
