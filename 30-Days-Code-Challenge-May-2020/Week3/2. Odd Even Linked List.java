6254b6d5433aa1c75bcf567462df0c0157c60262
Abanoub-Asaad <abanoub.asaad200010@gmail.com>
2020-05-16 16:09:56 +0200

    add another solution to problem 16

--- 30-Days-Code-Challenge-May-2020/Week3/2. Odd Even Linked List.java
+++ 30-Days-Code-Challenge-May-2020/Week3/2. Odd Even Linked List.java
@@ -50,3 +50,19 @@ class Solution {
     
     
 }
+
+//============== Time: O(n), Space: O(1) == LeetCode's Solution
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
