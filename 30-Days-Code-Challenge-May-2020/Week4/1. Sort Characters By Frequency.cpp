bbd219049371f7865c65ec608b0141aa691b1f3e
Abanoub-Asaad <abanoub.asaad200010@gmail.com>
2020-05-22 13:53:19 +0200

    add solution to p 22

---
+++ 30-Days-Code-Challenge-May-2020/Week4/1. Sort Characters By Frequency.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    
+    bool sortinrev(const pair<char,int> &a,  
+               const pair<char,int> &b) 
+    { 
+           return (a.first > b.first); 
+    } 
+    
+    string frequencySort(string s) {
+        
+        string output="";
+        vector<pair<char,int>>v;
+        map<char,int>mp;
+        map<char,int>::iterator it;
+        
+        for(int i=0; i<s.size(); i++)
+            ++mp[s[i]];
+        for(it=mp.begin(); it!=mp.end(); it++)
+            v.push_back(make_pair(it->first, it->second));
+        
+        //The lampda expression for sorting by value in descending order 
+        std::sort( v.begin(), v.end(),
+               []( const std::pair<char, int> &p1, const std::pair<char, int> &p2 )
+               {
+                   return ( p1.second > p2.second || 
+                          ( !( p2.second > p1.second ) && p1.first < p2.first ) );
+               } );
+        
+        for(int i=0; i<v.size(); i++)
+           for(int j=0; j<v[i].second; j++)
+               output+=v[i].first;
+    
+        return output;
+    }
+    
+};
+
+//=========== Another Solution
+class Solution {
+public:
+    
+    bool sortinrev(const pair<char,int> &a,  
+               const pair<char,int> &b) 
+    { 
+           return (a.first > b.first); 
+    } 
+    
+    string frequencySort( string s ) {
+	unordered_map<char, int> m;
+	for( auto c : s )
+		++m[c];
+
+	priority_queue<pair<int, char>> maxHeap;
+	for( auto p : m )
+		maxHeap.push( { p.second, p.first } );
+
+	string result = "";
+	while( !maxHeap.empty() ) {
+		auto p = maxHeap.top(); maxHeap.pop();
+		while( p.first-- )
+			result += p.second;
+	}
+	return result;
+}
+    
+};
---
+++ 30-Days-Code-Challenge-May-2020/Week4/README.md
@@ -0,0 +1,4 @@
+# Week 4
+Problems statements :
+
+1. Sort Characters By Frequency, click [HERE](hhttps://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3337/)
