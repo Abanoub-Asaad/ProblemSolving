ba36ceb751144b0c20b6984198050442ff41ce04
Abanoub-Asaad <abanoub.asaad@robustastudio.com>
2022-01-13 13:07:49 +0200

    feat: add problem description

--- Interviews/absolute_value_sort.cpp
+++ Interviews/absolute_value_sort.cpp
@@ -1,3 +1,12 @@
+/*
+Absolute Value Sort
+Given an array of integers arr, write a function absSort(arr), that sorts the array according to the absolute values of the numbers in arr. 
+If two numbers have the same absolute value, sort them according to sign, where the negative numbers come before the positive numbers.
+Examples:
+input:  arr = [2, -7, -2, -2, 0]
+output: [0, -2, -2, 2, -7]
+/*
+
 #include <iostream>
 #include <unordered_map>
 #include <vector>
